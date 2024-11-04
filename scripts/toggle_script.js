1ed15731351c382b2492a2c2b7d7ceec01a025c8
alilawati87 <alilawati87@gmail.com>
2024-11-04 21:06:32 +0400

    Update toggle_script.js

--- scripts/toggle_script.js
+++ scripts/toggle_script.js
@@ -1,14 +1,10 @@
-const button = document.getElementById('toggleButton');
-button.addEventListener('click', () => {
-    // Get the current background color of the body
-    const currentColor = window.getComputedStyle(document.body).backgroundColor;
-
-    // If the current color is not black, change it to black
-    if (currentColor !== 'rgb(0, 0, 0)') {
+function toggleBackgroundColor() {
+    if (document.body.style.backgroundColor !== 'black') {
         document.body.style.backgroundColor = 'black';
     } 
     // Otherwise, change it to white
     else {
         document.body.style.backgroundColor = 'white';
     }
-});
+
+};
