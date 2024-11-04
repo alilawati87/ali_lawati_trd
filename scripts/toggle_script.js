5b60c59339037ebd4051b94dc6a615677bcff3a5
alilawati87 <alilawati87@gmail.com>
2024-11-04 21:14:05 +0400

    Update toggle_script.js

--- scripts/toggle_script.js
+++ scripts/toggle_script.js
@@ -1,10 +1,9 @@
 function toggleBackgroundColor() {
     if (document.body.style.backgroundColor !== 'black') {
         document.body.style.backgroundColor = 'black';
-    } 
-    // Otherwise, change it to white
-    else {
+        document.body.style.color = '#e4b813'; // Set text color to #e4b813
+    } else {
         document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black'; // Set text color to black
     }
-
-};
+}
